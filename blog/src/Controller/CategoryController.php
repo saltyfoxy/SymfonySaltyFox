c81805b8961153af23f406db28497a80f016d95c
saltyfoxy <nerdgbx@gmail.com>
2019-06-03 16:50:05 +0200

    form symfony

---
+++ blog/src/Controller/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class CategoryController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @Route("/category", name="category_add")
+     * @return Response
+     */
+    public function add(Request $request) : Response
+    {
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($category);
+            $manager->flush();
+            $this->addFlash('success', 'Category added !');
+        }
+        return $this->render('Category/addCategory.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
