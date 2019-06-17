8e28c4f3281412d045c043ed787e29109d1c4dba
saltyfoxy <nerdgbx@gmail.com>
2019-06-17 09:54:16 +0200

    quete EntityType

--- blog/src/Form/ArticleType.php
+++ blog/src/Form/ArticleType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArticleType extends AbstractType
 {
@@ -15,6 +17,11 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,])
         ;
     }
 
