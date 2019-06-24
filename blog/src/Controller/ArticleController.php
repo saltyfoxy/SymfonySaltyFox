025a574f493713c5bbaabbb64464d75abe0a6cf6
saltyfoxy <nerdgbx@gmail.com>
2019-06-24 09:38:31 +0200

    quete services

--- blog/src/Controller/ArticleController.php
+++ blog/src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/article")
  */
-class ArticleController extends AbstractController
+class  ArticleController extends AbstractController
 {
     /**
      * @Route("/", name="article_index", methods={"GET"})
